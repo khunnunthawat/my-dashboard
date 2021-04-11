f4dfdd246b8be883926a2c3d8f68492d90472ed7
khunnunthawat <khunnunthawat@hotmail.com>
2021-04-12 01:49:59 +0700

    col

--- components/Widgets.js
+++ components/Widgets.js
@@ -9,17 +9,15 @@ class Widgets extends Component {
       <div className='w-100 max-w-4xl mx-auto p-5'>
         <h2 className='text-2xl undefined'>Widgets</h2>
         <div className='pt-3'>
-          <div className='md:masonry'>
-            <div className='grid grid-cols-6 gap-4'>
-              <div className='col-span-12 sm:col-span-6 md:col-span-3'>
-                <JustSaycard />
-              </div>
-              <div className='col-span-12 sm:col-span-6 md:col-span-3'>
-                <Timer />
-              </div>
-              <div className='col-span-12 sm:col-span-6 md:col-span-3'>
-                <Counter />
-              </div>
+          <div className='grid grid-cols-6 gap-4'>
+            <div className='col-span-12 sm:col-span-6 md:col-span-3'>
+              <JustSaycard />
+            </div>
+            <div className='col-span-12 sm:col-span-6 md:col-span-3'>
+              <Timer />
+            </div>
+            <div className='col-span-12 sm:col-span-6 md:col-span-3'>
+              <Counter />
             </div>
           </div>
         </div>
