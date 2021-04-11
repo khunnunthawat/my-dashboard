69e9583280b7e1ee618b37b808fb6ba91474ddbd
khunnunthawat <khunnunthawat@hotmail.com>
2021-04-12 01:54:45 +0700

    AM

--- components/Timer.js
+++ components/Timer.js
@@ -41,7 +41,7 @@ const Timer = () => {
   }
 
   return (
-    <div clasclassNames='md:break-inside pb-4'>
+    <div className='md:break-inside pb-4'>
       <div className='p-5 border-1 bg-white rounded-2xl'>
         <h2 className='text-lg font-bold text-gray-400 mb-1.5'>Timer</h2>
         <div className='text-center'>
