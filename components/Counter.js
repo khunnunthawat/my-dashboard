ae7bc4cfc43230c1474b5a41de6cbeb426250d2f
khunnunthawat <khunnunthawat@hotmail.com>
2021-04-12 00:03:19 +0700

    counter

--- components/Counter.js
+++ components/Counter.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 
-export default class Counter extends Component {
-  render() {
-    return (
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  let styled;
+  let decrease;
+
+  if (count <= 0) {
+    styled = (
+      <button
+        onClick={() => setCount(0)}
+        className='text-white focus:outline-none px-4 py-1 rounded-md bg-gray-300'
+      >
+        Reset
+      </button>
+    );
+    decrease = (
+      <button
+        onClick={() => setCount(0)}
+        className='text-5xl rounded-full w-10 text-center focus:outline-none text-gray-300'
+      >
+        -
+      </button>
+    );
+  } else {
+    styled = (
+      <button
+        onClick={() => setCount(0)}
+        className='text-white focus:outline-none px-4 py-1 rounded-md bg-blue-500 hover:bg-blue-600'
+      >
+        Reset
+      </button>
+    );
+    decrease = (
+      <button
+        onClick={() => setCount(count - 1)}
+        className='text-5xl rounded-full w-10 text-center focus:outline-none text-blue-600'
+      >
+        -
+      </button>
+    );
+  }
+
+  return (
+    <div>
       <div>
         <h2 className='text-lg font-bold text-gray-400 mb-1.5'>Counter</h2>
         <div className='text-center'>
           <div className='flex items-center justify-center mt-4 mb-6'>
-            <button className='text-5xl rounded-full w-10 text-center focus:outline-none text-gray-300'>
-              -
-            </button>
-            <div className='text-6xl mx-7'>0</div>
-            <button className='text-5xl rounded-full w-10 text-center focus:outline-none text-blue-500'>
+            {decrease}
+            <div className='text-6xl mx-7'>{count}</div>
+            <button
+              onClick={() => setCount(count + 1)}
+              className='text-5xl rounded-full w-10 text-center focus:outline-none text-blue-500'
+            >
               +
             </button>
           </div>
-          <button className=' text-white focus:outline-none px-4 py-1 rounded-md bg-gray-300'>
-            Reset
-          </button>
+          {styled}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
