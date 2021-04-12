0585b3e0a3466d89ca08e62ee8968864b6c42d8f
khunnunthawat <khunnunthawat@hotmail.com>
2021-04-12 14:30:37 +0700

    last_2_30_pm

--- components/JustSaycard.js
+++ components/JustSaycard.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 class JustSaycard extends Component {
   render() {
     return (
-      <div className='text-center my-8'>
-        <h1 className='text-4xl font-bold undefined'>Hello :)</h1>
+      <div className='md:row-start-1 pb-4'>
+        <div className='p-5 border-1 bg-white rounded-2xl'>
+          <h2 className='text-lg font-bold text-gray-400 mb-1.5'>JustSay</h2>
+          <div className='text-center my-8'>
+            <h1 className='text-4xl font-bold undefined'>Hello :)</h1>
+          </div>
+        </div>
       </div>
     );
   }
--- components/WidgetsCard.js
+++ components/WidgetsCard.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import JustSaycard from './JustSaycard'
 
 const WidgetsCard = ({ children }) => {
   return (
     <div className='md:row-start-1 pb-4'>
-      <div className='p-5 border-1 bg-white rounded-2xl'>
-        <h2 className='text-lg font-bold text-gray-400 mb-1.5'>JustSay</h2>
-        <JustSaycard />
-      </div>
+      {children}
     </div>
   );
 };
