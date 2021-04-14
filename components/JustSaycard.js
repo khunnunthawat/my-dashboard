0575070cc90d377e56548e61d9df28507218e05f
khunnunthawat <khunnunthawat@hotmail.com>
2021-04-14 17:35:25 +0700

    now

--- components/JustSaycard.js
+++ components/JustSaycard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Card from '../components/Card';
 
 export default function JustSaycard() {
+  const JustSay = "Hello :)";
   return (
     <Card title='Just Say'>
       <div className='md:break-inside pb-4'>
         <div className='text-center my-8'>
-          <h1 className='text-4xl font-bold'>{'Hello :)'}</h1>
+          <h1 className='text-4xl font-bold'>{ JustSay }</h1>
         </div>
       </div>
     </Card>
