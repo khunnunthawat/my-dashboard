e017b09a777284d4b242a84cbc195dcc7947f3cf
khunnunthawat <khunnunthawat@hotmail.com>
2021-04-21 13:39:52 +0700

    Phase2_complied

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Widget - Daytech Dashboard</title>
+        <title>Widgets - Daytech Dashboard</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <WidgetContent />
