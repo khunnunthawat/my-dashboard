96df82207bad65cb877c33d4dcd5dcd2d4f7f9f3
khunnunthawat <68588944+khunnunthawat@users.noreply.github.com>
2022-11-14 18:14:08 +0700

    Update MainLayout.js

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -4,7 +4,7 @@ export default function MainLayout({ children }) {
   return (
     <div className='min-h-screen bg-gray-200'>
       <div className='w-100 max-w-4xl mx-auto p-5'>
-        <h1 className='text-4xl font-bold'>Daytech Dashboard</h1>
+        <h1 className='text-4xl font-bold'>EIFFEL Dashboard</h1>
         <Page />
         {children}
       </div>
